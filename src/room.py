7d4cb0510a5dcd69b8e8ab65c33336586025d87c
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-08-07 12:03:29 -0500

    issue displaying available items

--- src/room.py
+++ src/room.py
@@ -2,9 +2,14 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, loot=[]):
         self.name = name
         self.description = description
+        self.loot = loot
 
     def __str__(self):
-        return f"location name: {self.name} : {self.description}"
+        return f"\nCurrent Location: {self.name} : {self.description}\n available loot: {self.loot}"
+
+    # def print_loot(self):
+    #     p = [f"{i.name} {i.description}" for i in {self.loot}]
+    #     print(p)
